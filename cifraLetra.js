b51307c37428df31eca00c09644465fedc642c65
cristianocsl <cslcristiano@gmail.com>
2021-10-10 14:24:42 -0300

    ajustes de filtro para notas bemol e notas solitarias

--- cifraLetra.js
+++ cifraLetra.js
@@ -4,32 +4,36 @@ function handleInput() {
   const texto = document.getElementById('input').value;
   
   const separarPorQuebraLinha = texto.split('\n'); // retorna um array
+
+  const linhaComEspaco = separarPorQuebraLinha.map((linha) => linha + ' ');
   
-  console.log(separarPorQuebraLinha);
+  console.log(linhaComEspaco);
 
   let filtrado;
-  filtrado = separarPorQuebraLinha.filter((linha) => !linha.includes('7'));
+  filtrado = linhaComEspaco.filter((linha) => !linha.includes('7'));
   filtrado = filtrado.filter((linha) => !linha.includes('5'));
   filtrado = filtrado.filter((linha) => !linha.includes('6'));
   filtrado = filtrado.filter((linha) => !linha.includes('9'));
   filtrado = filtrado.filter((linha) => !linha.includes('/'));
   filtrado = filtrado.filter((linha) => !linha.includes('#'));
+  filtrado = filtrado.filter((linha) => !linha.includes('E '));
   filtrado = filtrado.filter((linha) => !linha.includes('G '));
   filtrado = filtrado.filter((linha) => !linha.includes('Gm '));
-  // filtrado = filtrado.filter((linha) => !linha.includes('A '));
-  filtrado = filtrado.filter((linha) => !linha.includes('Am'));
+  filtrado = filtrado.filter((linha) => !linha.includes(' A '));
+  filtrado = filtrado.filter((linha) => !linha.includes('Am '));
   filtrado = filtrado.filter((linha) => !linha.includes('B '));
   filtrado = filtrado.filter((linha) => !linha.includes('Bm'));
   filtrado = filtrado.filter((linha) => !linha.includes('C '));
-  filtrado = filtrado.filter((linha) => !linha.includes('Cm '));
+  filtrado = filtrado.filter((linha) => !linha.includes('Cm'));
   filtrado = filtrado.filter((linha) => !linha.includes('A(sus2)'));
-  // filtrado = filtrado.filter((linha) => !linha.includes('D'));
-  // filtrado = filtrado.filter((linha) => !linha.includes('Dm '));
+  filtrado = filtrado.filter((linha) => !linha.includes('D '));
+  filtrado = filtrado.filter((linha) => !linha.includes('Dm'));
+  filtrado = filtrado.filter((linha) => !linha.includes('A ') || !linha.includes('E '));
   filtrado = filtrado.filter((linha) => !linha.includes('E ') || !linha.includes('A '));
   filtrado = filtrado.filter((linha) => !linha.includes('D ') || !linha.includes('A '));
-  filtrado = filtrado.filter((linha) => !linha.includes('Em') || !linha.includes('G'));
+  filtrado = filtrado.filter((linha) => !linha.includes('Em ') || !linha.includes('G '));
   // filtrado = filtrado.filter((linha) => !linha.includes('E'));
-  filtrado = filtrado.filter((linha) => !linha.includes(' Em ') || !linha.includes('D'));
+  filtrado = filtrado.filter((linha) => !linha.includes(' Em ') || !linha.includes('D '));
   filtrado = filtrado.filter((linha) => !linha.includes('F '));
   filtrado = filtrado.filter((linha) => !linha.includes('C°'));
   filtrado = filtrado.filter((linha) => !linha.includes('D°'));
@@ -38,6 +42,13 @@ function handleInput() {
   filtrado = filtrado.filter((linha) => !linha.includes('G°'));
   filtrado = filtrado.filter((linha) => !linha.includes('A°'));
   filtrado = filtrado.filter((linha) => !linha.includes('B°'));
+  filtrado = filtrado.filter((linha) => !linha.includes('Cb'));
+  filtrado = filtrado.filter((linha) => !linha.includes('Db'));
+  filtrado = filtrado.filter((linha) => !linha.includes('Eb'));
+  filtrado = filtrado.filter((linha) => !linha.includes('Fb'));
+  filtrado = filtrado.filter((linha) => !linha.includes('Gb'));
+  filtrado = filtrado.filter((linha) => !linha.includes('Ab'));
+  filtrado = filtrado.filter((linha) => !linha.includes('Bb'));
   filtrado = filtrado.map((linha) => linha + `</br>`);
 
 
